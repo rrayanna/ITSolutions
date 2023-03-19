09f789b920685313cb25b166e40c29efce3f9ebb
Raghu Rayannagari <15205277+rrayanna@users.noreply.github.com>
2023-03-19 15:35:58 -0700

    thank_you.php
    thank you

---
+++ thank_you.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Thank You | Singularity IT Solutions</title>
+    <link rel="stylesheet" href="styles.css" />
+  </head>
+  <body>
+    <header>
+      <nav>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About Us</a></li>
+          <li><a href="#">Services</a></li>
+          <li><a href="#">Case Studies</a></li>
+          <li><a href="#">Contact Us</a></li>
+        </ul>
+      </nav>
+      <div class="hero">
+        <h1>Thank You</h1>
+        <p>Your message has been sent.</p>
+      </div>
+    </header>
+    <main>
+      <section>
+        <h2>What's Next?</h2>
+        <p>We'll get back to you as soon as possible.</p>
+      </section>
+    </main>
+    <footer>
+      <p>&copy; 2023 Singularity IT Solutions. All rights reserved.</p>
+    </footer>
+  </body>
+</html>
